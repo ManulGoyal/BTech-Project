28e2bb3c340b1ae98de221254233e0fe5dcb1c93
ManulGoyal <goyal.10@iitj.ac.in>
2020-07-10 16:56:22 +0530

    semantic matrix wrapper

--- ML-GCN/models.py
+++ ML-GCN/models.py
@@ -41,7 +41,11 @@ class GraphConvolution(nn.Module):
 
 
 class GCNResnet(nn.Module):
-    def __init__(self, model, num_classes, in_channel=300, t=0, adj_file=None):
+    # adj_matrix = "normal" or "semantic"
+    # trick = 1 for A[i][j] *= (s[i]/s[j])^scalar
+    # trick = 2 for A[i][j] = (s[i]/s[j]) ^ scalar
+    # trick = 3 for A[i][j] *= (s[i]-s[j])
+    def __init__(self, model, num_classes, in_channel=300, t=0, adj_file=None, adj_matrix="normal", trick=1, scalar=1):
         super(GCNResnet, self).__init__()
         self.features = nn.Sequential(
             model.conv1,
@@ -60,7 +64,10 @@ class GCNResnet(nn.Module):
         self.gc2 = GraphConvolution(1024, 2048)
         self.relu = nn.LeakyReLU(0.2)           # Knob
 
-        _adj = gen_A(num_classes, t, adj_file)
+        if adj_matrix == "normal":
+            _adj = gen_A(num_classes, t, adj_file)
+        else:
+            _adj = gen_A_semantic(adj_file, trick, scalar)
         self.A = Parameter(torch.from_numpy(_adj).float())
         # image normalization
         self.image_normalization_mean = [0.485, 0.456, 0.406]
